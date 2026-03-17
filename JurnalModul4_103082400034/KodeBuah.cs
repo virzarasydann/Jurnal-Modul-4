875c6ea5d74cbb1af22c9934ac24cabfc90a5f10
virzarasydann <virzarasydan@gmail.com>
2026-03-17 14:00:37 +0700

    inisialisasi proyek

---
+++ JurnalModul4_103082400034/KodeBuah.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JurnalModul4_103082400034
+{
+    internal class KodeBuah
+    {
+        public static void getKodeBuah()
+        {
+            string[] kodeBuah = {"A00","B00","C00","D00","E00","F00","H00","I00","J00" };
+
+            for (int i = 0; i < kodeBuah.Length; i++)
+            {
+                Console.WriteLine("Kode Buah : "+kodeBuah[i]);
+            }
+
+
+
+        }
+            
+    }
+}
---
+++ JurnalModul4_103082400034/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JurnalModul4_103082400034
+{
+    internal class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            KodeBuah.getKodeBuah();
+        }
+    }
+}
